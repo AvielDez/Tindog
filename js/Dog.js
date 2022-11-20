9956ab2912883c6d22df35cd97cca91601de8e55
AvielDez <110438720+AvielDez@users.noreply.github.com>
2022-11-19 23:23:19 -0600

    Functional App Completed
    The App is fully functional and passes all the requirements.

---
+++ js/Dog.js
@@ -0,0 +1,35 @@
+class Dog {
+    constructor(data){
+        Object.assign(this, data)
+        this.badgeHtml = ``
+    }
+
+    getDogProfileHtml(){
+        const {name, avatar, age, bio, hasBeenSwiped, hasBeenLiked} = this
+
+        return `
+            <div class="dog-profile" style="background-image: url(${avatar})">
+                <div id="badge-container">
+                    ${this.badgeHtml}
+                </div>
+                    
+                <div class="dog-profile-text">
+                    <h2 class="name-age">${name}, ${age}</h2>
+                    <p class="bio">${bio}</p>
+                </div>
+            </div>
+        `
+    }
+
+    setLiked(){
+        this.hasBeenSwiped = true
+        this.hasBeenLiked = true
+        this.badgeHtml = `<img class="badge" src="../images/badge-like.png">`
+    }
+    setNope(){
+        this.hasBeenSwiped = true
+        this.badgeHtml = `<img class="badge" src="../images/badge-nope.png">`
+    }
+}
+
+export default Dog
--- js/app.js
+++ js/app.js
@@ -1,1 +1,46 @@
-// Create the Dog class here
+import Dog from './Dog.js'
+import dogs from './data.js'
+
+let dogProfile = getNewDogProfile()
+
+render()
+
+let isWaiting = false
+
+document.addEventListener('click', (event)=>{
+    if(event.target.id === 'nope-button'){
+        if(!isWaiting){
+            isWaiting = true
+            dogProfile.setNope()
+            console.log(dogProfile)
+            render()
+            setTimeout(()=>{
+                isWaiting = false
+                dogProfile = getNewDogProfile()
+                render()
+            }, 700)
+        }
+    }
+    else if(event.target.id === 'like-button'){
+        if(!isWaiting){
+            isWaiting = true
+            dogProfile.setLiked()
+            render()
+            console.log(dogProfile)
+            setTimeout(()=>{
+                isWaiting = false
+                dogProfile = getNewDogProfile()
+                render()
+            }, 700)
+        }
+    }
+})
+
+function render(){
+    document.getElementById('dog-profile-container').innerHTML = dogProfile.getDogProfileHtml()
+}
+
+function getNewDogProfile(){
+    const nextDogProfileData = dogs.shift()
+    return nextDogProfileData ? new Dog(nextDogProfileData) : []
+}
